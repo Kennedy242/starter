0767a0d6d84965769a8e13aa111397815fecd44c
Kayce Serafin <80504103+Kennedy242@users.noreply.github.com>
2025-02-21 14:38:24 -0700

    Add code folding
    Use ufo for folding and use config from https://productsway.com/blog/til-40-how-to-set-up-folding-in-neovim

--- lua/options.lua
+++ lua/options.lua
@@ -18,3 +18,10 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
     end
   end,
 })
+
+-- UFO folding
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
