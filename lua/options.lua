cf19d61cbe7451f00eeb2e08c918afcbd44d74ca
Kayce Serafin <80504103+Kennedy242@users.noreply.github.com>
2025-02-21 14:38:24 -0700

    Add restore cursor position
    - Except for git files

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,16 @@ vim.opt.mouse = "" -- disable mouse
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Restore cursor position
+-- except for git rebase and commit files
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  pattern = { "*" },
+  callback = function(args)
+    -- print("Args are: ", args.file)
+    if not string.find(args.file, "%.git") then
+      vim.api.nvim_exec2('silent! normal! g`"zv', {})
+      -- print "Restored cursor position"
+    end
+  end,
+})
