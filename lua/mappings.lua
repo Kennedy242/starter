83924a859e38d579d799aec2674258a11a4dea08
Kayce Serafin <80504103+Kennedy242@users.noreply.github.com>
2025-02-24 08:06:58 -0700

    Add: mappings
    Keep line centered on screen

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,7 @@
 require "nvchad.mappings"
 
 -- add yours here
+-- map(<mode>, <keystroke>, <command to run>, {<other lua things, in this case just giving it a description to display>})
 
 local map = vim.keymap.set
 
@@ -9,5 +10,8 @@ map("i", "jk", "<ESC>")
 
 map("n", "n", "nzz")
 map("n", "N", "Nzz")
+
+-- map("n", "j", "jzz")
+-- map("n", "k", "kzz")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
